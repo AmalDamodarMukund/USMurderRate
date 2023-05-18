77a3fbfea15e9cc470f9f62672fe5522d0f89cd7
Amal Mukund - STUDENT <l00171227@atu.ie>
2023-05-18 12:29:54 +0100

    initial commit

---
+++ from_proffesor_2.R
@@ -0,0 +1,410 @@
+# Practical 9
+
+# Q1
+states <- as.data.frame(state.x77)
+str(states)
+# Add the states name as a variable
+states$name <- state.name
+
+# Renaming Life Exp and HS Grad variables as 
+# these will cause possible issues when referring to
+# them since they contain a space.
+colnames(states)[colnames(states) == "Life Exp"] <- "Life_Exp"
+colnames(states)[colnames(states) == "HS Grad"] <- "HS_Grad"
+
+# Q3a
+# Examine initial linearity between variables in the dataset
+library(psych)
+pairs.panels(states,
+             smooth = FALSE,      # If TRUE, draws loess smooths
+             scale = FALSE,      # If TRUE, scales the correlation text font
+             density = TRUE,     # If TRUE, adds density plots and histograms
+             ellipses = FALSE,    # If TRUE, draws ellipses
+             method = "spearman",# Correlation method (also "pearson" or "kendall")
+             pch = 21,           # pch symbol
+             lm = FALSE,         # If TRUE, plots linear fit rather than the LOESS (smoothed) fit
+             cor = TRUE,         # If TRUE, reports correlations
+             jiggle = FALSE,     # If TRUE, data points are jittered
+             factor = 2,         # Jittering factor
+             hist.col = 4,       # Histograms color
+             stars = TRUE,       # If TRUE, adds significance level with stars
+             ci = TRUE)          # If TRUE, adds confidence intervals
+
+# Examine linearity in more detail
+scatter.smooth(x = states$Population,
+               y = states$Murder,
+               xlab = "Population (,000)",
+               ylab = "Murder %", main = "Correlation of murder ~ population")
+
+# chart appears to have medium correlation
+# low correlation (-0.2 < x < 0.2) suggests that 
+# much of variation of the response
+# variable (Y) is unexplained by the predictor (X)
+# in which case, we should probably look for better
+# explanatory variables
+cor(states$Murder, states$Population)
+
+scatter.smooth(x = states$Illiteracy,
+               y = states$Murder,
+               main = "Correlation of Murder ~ Illiteracy",
+               xlab = "Illiteracy %",
+               ylab = "Murder %")
+
+# Examining correlation between murder and illiteracy
+cor(states$Murder, states$Illiteracy)
+
+# This is a better correlation value between both variables.
+# Lets examine murder and frost variables for correlation.
+scatter.smooth(x = states$Frost,
+               y = states$Murder,
+               main = "Correlation of Murder ~ Frost",
+               xlab = "Frost",
+               ylab = "Murder %")
+cor(states$Murder, states$Frost)
+
+# Examining the other variables
+paste("Correlation for Murder and Frost: ", cor(states$Murder, states$Frost))
+paste("Correlation for Murder and Illiteracy: ", cor(states$Murder, states$Illiteracy))
+paste("Correlation for Murder and Population: ", cor(states$Murder, states$Population))
+paste("Correlation for Murder and HS Grad: ", cor(states$Murder, states$HS_Grad))
+paste("Correlation for Murder and Income: ", cor(states$Murder, states$Income))
+paste("Correlation for Murder and Life Exp: ", cor(states$Murder, states$Life_Exp))
+paste("Correlation for Murder and Area: ", cor(states$Murder, states$Area))
+
+# It appears that the variable Area has a vary low correlation with Murder. 
+# Therefore I am going to remove it from the dataset. 
+# Alternatively we can choose to exclude these independent variables when
+# we are constructing the MLR model.
+
+# Q3b
+# Check for outliers
+opar <- par(no.readonly = TRUE)
+par(mfrow = c(4, 2)) # divide graph area in 3 rows by 2 columns
+attach(states)
+boxplot(Murder,
+        main = "Murder",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(Murder)$out)) # box plot for 'murder'
+boxplot(Population,
+        main = "Population",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(Population)$out)) # box plot for 'Population'
+boxplot(states$HS_Grad,
+        main = "Graduation",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(states$HS_Grad)$out)) # box plot for 'HS Grad'
+boxplot(Illiteracy,
+        main = "Illiteracy",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(Illiteracy)$out)) # box plot for 'HS Grad'
+boxplot(Income,
+        main = "Income",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(Income)$out)) # box plot for 'HS Grad'
+boxplot(Frost,
+        main = "Frost",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(Frost)$out)) # box plot for 'HS Grad'
+boxplot(states$Life_Exp,
+        main = "Life Exp",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(states$Life_Exp)$out)) # box plot for 'HS␣Grad'
+detach(states)
+par(opar)
+
+# Both the population and Income variables contain outliers.
+# Use boxplot.stats() function to generate relevant outliers
+outlier_values <- boxplot.stats(states$Population)$out # outlier values.
+paste("Population outliers: ", paste(outlier_values, collapse=", "))
+
+# Use boxplot.stats() function to generate relevant outliers
+outlier_values <- boxplot.stats(states$Income)$out # outlier values.
+paste("Income outliers: ", paste(outlier_values, collapse=", "))
+
+# Remove population outliers
+states <- subset(states,
+                 states$Population != 21198
+                 & states$Population != 11197
+                 & states$Population != 18076
+                 & states$Population != 11860
+                 & states$Population != 12237)
+# Remove Income outliers
+states <- subset(states, states$Income != 6315)
+# Remove Area outliers
+states <- subset(states,
+                 states$Area != 566432
+                 & states$Population != 156361
+                 & states$Population != 262134)
+
+# Re-run the box-plots to verify that outliers have now gone.
+
+# Q3c
+# Check for normality
+# Skewness function to examine normality
+# install.packages("e1071")
+library(e1071)
+opar <- par(no.readonly = TRUE)
+par(mfrow = c(4,2)) # divide graph area into 1 row x 2 cols
+
+# skewness of < -1 or > 1 = highly skewed
+# -1 to -0.5 and 0.5 to 1 = moderately skewed
+# Skewness of -0.5 to 0.5 = approx symetrical
+plot(density(states$Population),
+     main = "Density plot : Population",
+     ylab = "Frequency", xlab = "Population",
+     sub = paste("Skewness : ", round(e1071::skewness(states$Population), 2)))
+# fill the area under the plot
+polygon(density(states$Population), col = "red")
+
+plot(density(states$Murder),
+     main = "Density plot : Murder",
+     ylab = "Frequency", xlab = "Murder",
+     sub = paste("Skewness : ", round(e1071::skewness(states$Murder), 2)))
+polygon(density(states$Murder), col = "red")
+
+plot(density(states$HS_Grad),
+     main = "Density plot : HS grade",
+     ylab = "Frequency", xlab = "HS grade",
+     sub = paste("Skewness : ", round(e1071::skewness(states$HS_Grad), 2)))
+# fill the area under the plot
+polygon(density(states$HS_Grad), col = "red")
+plot(density(states$Illiteracy),
+     main = "Density plot : Illiteracy",
+     ylab = "Frequency", xlab = "Illiteracy",
+     sub = paste("Skewness : ", round(e1071::skewness(states$Illiteracy), 2)))
+polygon(density(states$Illiteracy), col = "red")
+plot(density(states$Income),
+     main = "Density plot : Income",
+     ylab = "Frequency", xlab = "Income",
+     sub = paste("Skewness : ", round(e1071::skewness(states$Income), 2)))
+# fill the area under the plot
+polygon(density(states$Income), col = "red")
+plot(density(states$Frost),
+     main = "Density plot : Frost",
+     ylab = "Frequency", xlab = "Feost",
+     sub = paste("Skewness : ", round(e1071::skewness(states$Frost), 2)))
+# fill the area under the plot
+polygon(density(states$Frost), col = "red")
+par(opar)
+
+# Minimal skewness = -0.11 - slightly skewed to the left. 
+# NB a skewness value <-1 or >1 = highly skewed. 
+# Skewness -1 to -05 and 0.5 to 1 = moderately skewed. 
+# And skewness -0.5 to 0-5 = approx symetric.
+# Illiteracy = 0.87 = moderatly skewed
+# Population = 1.15 = highly skewed
+# all others seem okay
+paste("Skewness for illiteracy : ", round(e1071::skewness(states$Illiteracy), 2))
+paste("Skewness for population : ", round(e1071::skewness(states$Population), 2))
+paste("Skewness for murder : ", round(e1071::skewness(states$Murder), 2))
+paste("Skewness for HS grad : ", round(e1071::skewness(states$HS_Grad), 2))
+paste("Skewness for income : ", round(e1071::skewness(states$Income), 2))
+paste("Skewness for frost : ", round(e1071::skewness(states$Frost), 2))
+
+# It seems that population must be converted
+# Data is visually skewed to the right
+hist(states$Population)
+
+# p-value indices that the data is not normally distributed
+shapiro.test(states$Population)
+
+# Check normality of the other variables
+shapiro.test(states$Illiteracy)
+shapiro.test(states$Murder)
+shapiro.test(states$HS_Grad)
+shapiro.test(states$Income)
+shapiro.test(states$Frost)
+
+# If p-value < 0.05 then variable
+# is not normally distributed
+
+# Illiteracy is not normally distributed (p-value = 8.297e-05)
+# Murder is normally distributed (p-value = 0.06601)
+# HS_grad is not normally distributed (p-value = 0.02194)
+# Income is normally distributed (p-value = 0.3246)
+# Frost is normally distributed (p-value = 0.0928)
+
+# Need to transform illiteracy, HS_grad and population
+#Find optimal values of lambda to transform the data
+
+install.packages("MASS")
+library(MASS)
+#plot the lambda from -6 to 6 in steps of 0.1
+Box <- boxcox(states$Population ~ 1,
+              lambda = seq(-6, 6, 0.1))
+
+#Create a dataframe containing the results
+Cox <- data.frame(Box$x, Box$y)
+
+#Order the new dataframe by decreasing y
+cox_smallest_y <- Cox[with(Cox, order(-Cox$Box.y)),]
+
+# log likelihood
+cox_smallest_y[1,]
+
+#extract lambda
+lambda <- cox_smallest_y[1, "Box.x"]
+lambda
+attach(states)
+# Tukey ladder of powers transformation
+transformed_population <- (Population ^ lambda - 1)/ lambda
+
+#Check for normality
+hist(transformed_population)
+shapiro.test(transformed_population)
+shapiro.test(states$Population)
+
+# Transform HS_Grad
+
+#plot the lambda from -6 to 6 in steps of 0.1
+Box <- boxcox(states$HS_Grad ~ 1,
+              lambda = seq(-6, 6, 0.1))
+
+#Create a dataframe containing the results
+Cox <- data.frame(Box$x, Box$y)
+
+#Order the new dataframe by decreasing y
+cox_smallest_y <- Cox[with(Cox, order(-Cox$Box.y)),]
+
+# log likelihood
+cox_smallest_y[1,]
+
+#extract lambda
+lambda <- cox_smallest_y[1, "Box.x"]
+lambda
+attach(states)
+transformed_HS_Grad <- (HS_Grad ^ lambda - 1)/ lambda
+
+#Check for normality
+hist(transformed_HS_Grad)
+shapiro.test(transformed_HS_Grad)
+#to see old value
+#shapiro.test(states$HS_Grad)
+
+# Transform Illiteracy
+
+#plot the lambda from -6 to 6 in steps of 0.1
+Box <- boxcox(states$Illiteracy ~ 1,
+              lambda = seq(-6, 6, 0.1))
+
+#Create a dataframe containing the results
+Cox <- data.frame(Box$x, Box$y)
+
+#Order the new dataframe by decreasing y
+cox_smallest_y <- Cox[with(Cox, order(-Cox$Box.y)),]
+
+# log likelihood
+cox_smallest_y[1,]
+
+#extract lambda
+lambda <- cox_smallest_y[1, "Box.x"]
+lambda
+attach(states)
+transformed_Illiteracy <- (Illiteracy ^ lambda - 1)/ lambda
+
+#Check for normality
+#to see old value
+#shapiro.test(states$Illiteracy)
+hist(transformed_Illiteracy)
+shapiro.test(transformed_Illiteracy)
+# p-value = 0.004267 - Illiteracy is still not normally distributed
+
+# Insert transformed variables into the states data frame
+states$transformed_population <- transformed_population
+states$transformed_Illiteracy <- transformed_Illiteracy
+states$transformed_HS_Grad <- transformed_HS_Grad
+
+#Split the data into training and testing
+set.seed(1)
+no_rows_data <- nrow(states)
+sample <- sample(1:no_rows_data, 
+                 size = round(0.7 * no_rows_data),
+                 replace = FALSE)
+training_data <- states[sample,]
+testing_data <- states[-sample,]
+
+#Build 2 models - 1 = not transformed data, 2 = transformed data
+
+#not transformed data
+unmodified_model <- lm(Murder ~ Population +
+                         Illiteracy +
+                         Life_Exp +
+                         HS_Grad + 
+                         Frost, 
+                       data = training_data)
+
+summary(unmodified_model)
+
+#transformed data
+modified_model <- lm(Murder ~ transformed_population +
+                         transformed_Illiteracy +
+                         Life_Exp +
+                         transformed_HS_Grad + 
+                         Frost, 
+                     data = training_data)
+
+summary(modified_model)
+
+#Evaluate the models
+#not transformed model evaluation
+install.packages("leaps")
+library(leaps)
+MLR_subset_selection <- regsubsets(Murder ~ Population +
+                                     Illiteracy +
+                                     Life_Exp +
+                                     HS_Grad + 
+                                     Frost, 
+                                   data = training_data,
+                                   nbest = 6)
+
+# adjr2 means adjusted r^2 value
+plot(MLR_subset_selection, scale = "adjr2")
+
+#transformed model evaluation
+MLR_subset_selection_modified <- regsubsets(Murder ~ transformed_population +
+                                     transformed_Illiteracy +
+                                     Life_Exp +
+                                     transformed_HS_Grad + 
+                                     Frost, 
+                                   data = training_data,
+                                   nbest = 6)
+
+# adjr2 means adjusted r^2 value
+plot(MLR_subset_selection_modified, scale = "adjr2")
+
+#Smallest AIC model is best
+stepAIC(unmodified_model, direction = "backward")
+
+stepAIC(modified_model, direction = "backward")
+
+#unmodified model vs modified model? - Not much difference
+
+#which variables to keep in the model?
+#Using population, illiteracy, life_exp, hs_grad
+#Drop frost - because believing that there is no correlation, only causation
+
+murder_model <- lm(Murder ~ Population + 
+                     Illiteracy +
+                     Life_Exp +
+                     HS_Grad,
+                   data = training_data)
+
+library(car)
+qqPlot(murder_model, 
+       labels = row.names(training_data$name), 
+       id.method = "identify",
+       simulate = TRUE,
+       main = "Q-Q plot for murder model")
+
+
+training_data["Alabama",]
+
+fitted(murder_model)["Alabama"]
+
+
+
+
+
+
+
+
